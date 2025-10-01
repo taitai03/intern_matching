58662272d4f57d573fca54e95d89d828c156dc05
taitai03 <ttatatai.gagaga@gmail.com>
2025-10-01 14:16:34 +0900

    make users's controller

---
+++ backend/app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < ApplicationController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: { user: resource, message: 'Signed up successfully.' }, status: :ok
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
